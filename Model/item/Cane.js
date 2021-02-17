a16a46136f8c176897a76a326dec16e4eb37c83e
あーる <>
2021-02-17 21:31:27 +0900

    杖をオブジェクト化して杖のコマンドをSteinと同期させないように修正

---
+++ Model/item/Cane.js
@@ -0,0 +1,255 @@
+const CaneNameAndIdList = [{
+        id: 1,
+        name: "ふきとばしの杖",
+        bid_price: 500,
+        selling_price: 250,
+        bid_increase: 25,
+        selling_increase: 12.5,
+        min_count: 4,
+        max_count: 7
+    },
+    {
+        id: 2,
+        name: "のりうつりの杖",
+        bid_price: 500,
+        selling_price: 250,
+        bid_increase: 25,
+        selling_increase: 12.5,
+        min_count: 4,
+        max_count: 6
+    },
+    {
+        id: 3,
+        name: "場所がえの杖",
+        bid_price: 800,
+        selling_price: 400,
+        bid_increase: 40,
+        selling_increase: 20,
+        min_count: 4,
+        max_count: 7
+    },
+    {
+        id: 4,
+        name: "引きよせの杖",
+        bid_price: 800,
+        selling_price: 400,
+        bid_increase: 40,
+        selling_increase: 20,
+        min_count: 4,
+        max_count: 7
+    },
+    {
+        id: 5,
+        name: "回復の杖",
+        bid_price: 800,
+        selling_price: 400,
+        bid_increase: 80,
+        selling_increase: 40,
+        min_count: 4,
+        max_count: 6
+    },
+    {
+        id: 6,
+        name: "ころばぬ先の杖",
+        bid_price: 1000,
+        selling_price: 500,
+        bid_increase: 50,
+        selling_increase: 25,
+        min_count: 0,
+        max_count: 1
+    },
+    {
+        id: 7,
+        name: "物知りの杖",
+        bid_price: 1000,
+        selling_price: 500,
+        bid_increase: 50,
+        selling_increase: 25,
+        min_count: 5,
+        max_count: 7
+    },
+    {
+        id: 8,
+        name: "クオーターの杖",
+        bid_price: 1200,
+        selling_price: 250,
+        bid_increase: 60,
+        selling_increase: 12.5,
+        min_count: 4,
+        max_count: 7
+    },
+    {
+        id: 9,
+        name: "鈍足の杖",
+        bid_price: 1200,
+        selling_price: 400,
+        bid_increase: 60,
+        selling_increase: 20,
+        min_count: 3,
+        max_count: 6
+    },
+    {
+        id: 10,
+        name: "封印の杖",
+        bid_price: 2000,
+        selling_price: 500,
+        bid_increase: 100,
+        selling_increase: 25,
+        min_count: 3,
+        max_count: 6
+    },
+    {
+        id: 11,
+        name: "トンネルの杖",
+        bid_price: 2000,
+        selling_price: 1000,
+        bid_increase: 100,
+        selling_increase: 50,
+        min_count: 3,
+        max_count: 7
+    },
+    {
+        id: 12,
+        name: "とうめいの杖",
+        bid_price: 2000,
+        selling_price: 1000,
+        bid_increase: 100,
+        selling_increase: 50,
+        min_count: 3,
+        max_count: 7
+    },
+    {
+        id: 13,
+        name: "倍速の杖",
+        bid_price: 2000,
+        selling_price: 1000,
+        bid_increase: 200,
+        selling_increase: 100,
+        min_count: 3,
+        max_count: 7
+    },
+    {
+        id: 14,
+        name: "からぶりの杖",
+        bid_price: 2500,
+        selling_price: 2500,
+        bid_increase: 125,
+        selling_increase: 60,
+        min_count: 3,
+        max_count: 6
+    },
+    {
+        id: 15,
+        name: "火ばしらの杖",
+        bid_price: 2500,
+        selling_price: 2500,
+        bid_increase: 125,
+        selling_increase: 60,
+        min_count: 4,
+        max_count: 7
+    },
+    {
+        id: 16,
+        name: "不幸の杖",
+        bid_price: 2500,
+        selling_price: 1000,
+        bid_increase: 250,
+        selling_increase: 100,
+        min_count: 4,
+        max_count: 6
+    },
+    {
+        id: 17,
+        name: "しあわせの杖",
+        bid_price: 3000,
+        selling_price: 1000,
+        bid_increase: 150,
+        selling_increase: 50,
+        min_count: 4,
+        max_count: 6
+    },
+    {
+        id: 18,
+        name: "草投げの杖",
+        bid_price: 3000,
+        selling_price: 1200,
+        bid_increase: 150,
+        selling_increase: 60,
+        min_count: 4,
+        max_count: 7
+    },
+    {
+        id: 19,
+        name: "草受けの杖",
+        bid_price: 3000,
+        selling_price: 1400,
+        bid_increase: 150,
+        selling_increase: 70,
+        min_count: 4,
+        max_count: 7
+    },
+];
+
+/**
+ * 杖のクラス
+ */
+class Cane {
+    /**
+     * Cane constructor
+     * @param string name アイテム名
+     */
+    constructor(name) {
+        this.cane = CaneNameAndIdList.find((v) => v.name === name);
+    }
+
+    /**
+     * 名前を取得
+     */
+    getName() {
+        return this.cane.name;
+    }
+
+    /**
+     * 杖の最大回数を取得する
+     */
+    getMaxCount() {
+        return this.cane.max_count;
+    }
+
+    /**
+     * 杖の最小回数を取得する
+     */
+    getMinCount() {
+        return this.cane.min_count;
+    }
+
+    /**
+     * 購入時の最高額を取得
+     */
+    getMaxBidPrice() {
+        return this.cane.bid_price + (this.cane.max_count * this.cane.bid_increase);
+    }
+
+    /**
+     * 購入時の最低額を取得
+     */
+    getMinBidPrice() {
+        return this.cane.bid_price + (this.cane.min_count * this.cane.bid_increase);
+    }
+
+    /**
+     * 購入時の最高額を取得
+     */
+    getMaxSellingPrice() {
+        return this.cane.selling_price + (this.cane.max_count * this.cane.bid_increase);
+    }
+
+    /**
+     * 購入時の最低額を取得
+     */
+    getMinSellingPrice() {
+        return this.cane.selling_price + (this.cane.min_count * this.cane.bid_increase);
+    }
+}
+
+module.exports = Cane
--- commands/item/CaneCommand.js
+++ commands/item/CaneCommand.js
@@ -1,87 +1,9 @@
 const commando = require('discord.js-commando')
 require('dotenv').config();
-const GetItem = require('../../stein/GetItem');
 const {
     MessageEmbed
 } = require('discord.js');
-
-const CaneNameAndIdList = [{
-        id: 1,
-        name: "ふきとばしの杖"
-    },
-    {
-        id: 2,
-        name: "のりうつりの杖"
-    },
-    {
-        id: 3,
-        name: "場所がえの杖"
-    },
-    {
-        id: 4,
-        name: "引きよせの杖"
-    },
-    {
-        id: 5,
-        name: "回復の杖"
-    },
-    {
-        id: 6,
-        name: "ころばぬ先の杖"
-    },
-    {
-        id: 7,
-        name: "物知りの杖"
-    },
-    {
-        id: 8,
-        name: "クオーターの杖"
-    },
-    {
-        id: 9,
-        name: "鈍足の杖"
-    },
-    {
-        id: 10,
-        name: "封印の杖"
-    },
-    {
-        id: 11,
-        name: "トンネルの杖"
-    },
-    {
-        id: 12,
-        name: "とうめいの杖"
-    },
-    {
-        id: 13,
-        name: "倍速の杖"
-    },
-    {
-        id: 14,
-        name: "からぶりの杖"
-    },
-    {
-        id: 15,
-        name: "火ばしらの杖"
-    },
-    {
-        id: 16,
-        name: "不幸の杖"
-    },
-    {
-        id: 17,
-        name: "しあわせの杖"
-    },
-    {
-        id: 18,
-        name: "草投げの杖"
-    },
-    {
-        id: 19,
-        name: "草受けの杖"
-    },
-];
+const Cane = require('../../Model/item/Cane');
 
 class CaneCommand extends commando.Command {
     /**
@@ -109,20 +31,23 @@ class CaneCommand extends commando.Command {
     async run(message, {
         name
     }) {
-        // 何故か日本語の検索ができないので仕方なくidと日本語を紐つけて記載
-        const target = CaneNameAndIdList.find((v) => v.name === name);
-        const store = new GetItem();
-        var cane = await store.get('Cane', String(target.id));
-        console.log(cane);
-        var maxBidPrice = Number(cane.bid_price) + Number((cane.max_count) * Number(cane.bid_increase));
-        var minBidPrice = Number(cane.bid_price) + (Number(cane.min_count) * Number(cane.bid_increase));
+        const cane = new Cane(name);
 
-        var maxSellingPrice = Number(cane.selling_price) + Number((cane.max_count) * Number(cane.bid_increase));
-        var minSellingPrice = Number(cane.selling_price) + Number((cane.min_count) * Number(cane.bid_increase));
+        var maxBidPrice = cane.getMaxBidPrice();
+        var minBidPrice = cane.getMinBidPrice();
+
+        var maxSellingPrice = cane.getMaxSellingPrice();
+        var minSellingPrice = cane.getMinSellingPrice()
+        var minCount = cane.getMinCount();
+        var maxCount = cane.getMaxCount();
+        // 説明文作成
+        var description = `買値:${minBidPrice}〜${maxBidPrice}\n` +
+            `売値:${minSellingPrice}〜${maxSellingPrice}\n` +
+            `${minCount}回〜${maxCount}回で出現するよ`;
         const embed = new MessageEmbed()
-            .setTitle(cane.name)
+            .setTitle(cane.getName())
             .setColor("#5d62ff")
-            .setDescription(`買値:${minBidPrice}〜${maxBidPrice}\n売値:${minSellingPrice}〜${maxSellingPrice}\n${cane.min_count}回〜${cane.max_count}回で出現するよ`)
+            .setDescription(description)
             .setTimestamp();
         return message.channel.send(embed)
     }
